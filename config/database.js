280ea9e4e388fcaea072ca8c5156d786ec98b707
JohnMichelin <jmichelinphone@gmail.com>
2017-07-10 14:05:30 -0400

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,5 +2,5 @@
  * Created by jmichelin on 3/24/17.
  */
 module.exports = {
-  'url' : 'mongodb://project-thesis:fuzzybunnyslippers@ds139470.mlab.com:39470/project-thesis-auth'
+  'url' : 'mongodb://project-thesis:pw@ds139470.mlab.com:39470/project-thesis-auth'
 };
